66c480757f8f5732a952e35881c03fc5a74eea88
qzshen <quinn.z.shen@gmail.com>
2013-10-10 22:50:42 -0700

    dc3, before step 3, no trailing $

--- dc3.py
+++ dc3.py
@@ -1,10 +1,12 @@
 # Quinn Z Shen
 # CS 176; dc3 implementation
 
+import string
 import radix
 
+LEXICALGRAPHICAL_ALPHABET = '$' + string.ascii_uppercase + string.ascii_lowercase
+
 def dc3(input):
-    print radix.radix_sort(list(set(i for i in input)), 10)
     b_0, b_1, b_2 = [], [], []
 
     for i in xrange(len(input)):
@@ -16,63 +18,114 @@ def dc3(input):
             b_2.append(i)   
 
     c = b_1 + b_2
+    print "c"
+    print c
 
-    input += "$$" #To ensure that each string is a triple towards the end.
-    r_1 = [input[x:x+3] for x in b_1]
-    r_2 = [input[x:x+3] for x in b_2]
-    input = input[:-2] #TODO: More efficient method?
+    input.extend([0, 0]) # Ensure that each string is a triple
+    r_1 = [tuple(input[x:x+3]) for x in b_1]
+    r_2 = [tuple(input[x:x+3]) for x in b_2]
+    input = input[:-2]
 
     r = r_1 + r_2
 
-    r_prime = radix.radix_sort(r, len(set(list(r))))
+    print "r"
+    print r
+
+    r_prime = radix.radix_sort(r, max(input))
+
+    print "r_prime"
     print r_prime
 
-    # print rank(radix_sort(r), r)
+    a_dict = rank(r, r_prime)
+    print "a_dict"
+    print a_dict
+    b_dict = {}
+    counter = 0
+    for x in c:
+        b_dict[x] = a_dict[counter]
+        counter += 1
+    b_dict[len(input) + 1] = 0
+    b_dict[len(input) + 2] = 0
+    print "b_dict"
+    print b_dict
 
+    b0_dict = {}
+    for x in b_0:
+        b0_dict[x] = (input[x], b_dict[x + 1])
+    print "b0_dict"
+    print b0_dict
 
+    # if not(unique(r_prime)):
+    #     r_prime = dc3(r_prime)
 
-def rank(input, r):
-    rank_ref = dict()
-    rank = 1
-    output = []
-    for i in input:
-        if not(i in rank_ref):
-            rank_ref[i] = rank
-            rank += 1
-    for i in r:
-        output.append(rank_ref[i])
-    return output
+def unique(input):
+    previous = None
+    for x in input:
+        if x == previous:
+            return False
+        previous = x
+    return True
 
-# def split(input, digit_num):
-#     tmp = set()
-#     unique_chars = []
-#     for x in input:
-#         if not(x[digit_num] in tmp):
-#             tmp.add(x[digit_num])
-#             unique_chars.append(x[digit_num])
-#     unique_chars.sort()
+def rank(r, r_prime):
+    counter = 0
+    a_set = set()
+    a_map = dict()
+    r_prime_rank = []
 
-#     bucket_ref = dict(zip(unique_chars, [x for x in range(len(unique_chars))]))
-#     buckets = [[] for x in range(len(unique_chars))]
-#     for char in input:
-#         buckets[bucket_ref[char[digit_num]]].append(char)
-#     return buckets
+    for x in r_prime:
+        if not (x in a_set):
+            a_set.add(x)
+            counter += 1
+            a_map[x] = counter
 
-# def merge(input):
-#     output = []
-#     for sublist in input:
-#         output.extend(sublist)
-#     return output
+    print a_map
 
-# def radix_sort(input):
-#     passes = 3
-#     output = input
-#     for i in range(passes)[::-1]:
-#         output = merge(split(output, i))
-#     return output
+    for x in r:
+        r_prime_rank.append(a_map[x])
+
+    print "len(a_set)"
+    print len(a_set)
+    print "len(r)"
+    print len(r)
+    print "r_rank"
+    print r_prime_rank
+
+    if len(a_set) != len(r):
+        print "~~~recursion~~~"
+        # r_prime_suffix_array = dc3(r_rank)
+        r_prime_suffix_array = [8, 0, 1, 6, 4, 2, 5, 3, 7][1:]
+        a_dict = {r_prime_suffix_array[i]: (i + 1) for i in r_prime_suffix_array}
+    else:
+        a_dict = {i:r_prime_rank[i] for i in xrange(len(r_prime_rank))}
+    
+    return a_dict
+
+    
+    return 
+    # previous = None
+    # r_rank = []
+    # r_prime_rank = []
+    # for x in range(len(r_prime)):
+    #     if r_prime[x] != previous:
+    #         counter += 1
+    #         previous = r_prime[x]
+    #     r_prime_rank.append(counter)
+    # print r_prime
+    # print r_prime_rank
+
+def to_integer_alphabet(string):
+    character_set = set(x for x in string)
+    character_mapping = {}
+    for char in LEXICALGRAPHICAL_ALPHABET:
+        if char in character_set:
+            character_mapping[char] = len(character_mapping) + 1
+    return [character_mapping[i] for i in string]
+
 
 def main():
-    dc3("yabbadabbado")
+    input_string = to_integer_alphabet("yabbadabbado")
+    print input_string
+    dc3(input_string)
 
 if __name__ == "__main__":
     main()
